ecda70e12aa0e10e536afa34d415b552df579ee8
Suhur Omar Farah <itssuhuromar@gmail.com>
2020-03-11 17:59:37 +0000

    snake changes

--- play.js
+++ play.js
@@ -5,9 +5,12 @@ const net = require('net');
  */
 const connect = function() {
     const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+        host: 'localhost',
+        port: 50541   
 });
+    conn.on("data", (data) => {
+        console.log(data)
+    })
   // interpret incoming data as text
 conn.setEncoding('utf8'); 
 
